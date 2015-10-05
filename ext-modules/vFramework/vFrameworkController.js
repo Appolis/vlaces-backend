3b9a24a2474663b94b34109ed4220750b828cc80
Alejandro Orrego L <aorrego@puntoticket.cl>
2015-10-05 19:00:12 -0300

    cambiods menu

--- ext-modules/vFramework/vFrameworkController.js
+++ ext-modules/vFramework/vFrameworkController.js
@@ -4,29 +4,33 @@
 "use strict";
 
 angular.module("vFramework").controller("vFrameworkController",
-    ["$scope", "$window",
-        function($scope, $window){
+    ["$scope", "$window", "$timeout",
+        function($scope, $window, $timeout){
             $scope.isMenuVisible = true;
             $scope.isMenuButtonVisible = true;
 
-            $scope.$on('v-menu-item-selected-event', function(evt, data){
+            $scope.$on("v-menu-item-selected-event", function(evt, data){
                 $scope.routeString = data.route;
             });
 
-            $($window).on('resize.vFramework', function(){
+            $($window).on("resize.vFramework", function(){
                 $scope.$apply(function(){
                     checkWidth();
                 });
             });
 
-            $scope.$on('$destroy', function(){
-                $($window).off('resize.vFramework');
+            $scope.$on("$destroy", function(){
+                $($window).off("resize.vFramework");
             });
 
             var checkWidth = function(){
-                var width = $($window).innerWidth();
-                $scope.isMenuVisible = (width > 768)
+                var width = Math.max( $($window).width(), $window.innerWidth );
+                $scope.isMenuVisible = (width >= 768);
                 $scope.isMenuButtonVisible = !$scope.isMenuVisible;
             };
+
+            $timeout( function(){
+                checkWidth();
+            }, 0);
         }
     ]);
